b70344255bc3c0bc6adaa41ede246c5878372162
Walker-Gideon <gideonawortwe3@gmail.com>
2025-04-07 21:59:33 +0000

    Added the menu component to the header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,30 @@
-function Header() {
-  return (
-    <header className="flex items-center justify-between py-4 px-8">
-      <a href="#" className="text-xl cursor-pointer">
-        Logo
-      </a>
+import { useState } from "react";
+import MenuNav from "./MenuNav";
 
-      <button className="border-1 border-white rounded-4xl px-5 py-1.5 text-lg text-center cursor-pointer">
-        menu
-      </button>
-    </header>
+function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  function handleMenu() {
+    setShowMenu((show) => !show);
+  }
+
+  return (
+    <div className="relative">
+      <header className="flex items-center justify-between py-4 px-8">
+        <a href="#" className="text-xl cursor-pointer">
+          Logo
+        </a>
+
+        <button
+          className="border-1 border-white rounded-4xl px-5 py-1.5 text-lg text-center cursor-pointer z-50"
+          onClick={handleMenu}
+        >
+          menu
+        </button>
+      </header>
+
+      {showMenu && <MenuNav />}
+    </div>
   );
 }
 
---
+++ src/components/header/MenuNav.jsx
@@ -0,0 +1,17 @@
+function MenuNav() {
+  return (
+    <aside className="absolute top-2 w-[98%] h-screen mx-4 bg-white text-black z-10">
+      <div className="flex flex-col px-6 pt-4">
+        <h1>Menu</h1>
+        <a href="#">menu</a>
+        <a href="#">menu</a>
+        <a href="#">menu</a>
+        <a href="#">menu</a>
+        <a href="#">menu</a>
+        <a href="#">menu</a>
+      </div>
+    </aside>
+  );
+}
+
+export default MenuNav;
