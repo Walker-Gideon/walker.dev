8ec56f414f3a6a2ba8a20e183a85bb245ac94200
Walker-Gideon <gideonawortwe3@gmail.com>
2025-04-11 20:28:10 +0000

    Fix some styling in the nav and menu components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,6 @@ function Header() {
   return (
     <div className="flex items-center justify-between w-full h-16 sticky top-0 z-50">
       <header className="relative flex items-center justify-between w-full px-8 py-4">
-        {/* <div > */}
         <a
           href="#"
           className={`text-xl ${showMenu === true ? `` : `cursor-pointer`}`}
@@ -19,21 +18,17 @@ function Header() {
           Logo
         </a>
 
-        <div className="relative">
-          <button
-            className={`border-1 rounded-4xl px-5 py-1.5 text-lg text-center cursor-pointer ${
-              showMenu === true
-                ? `border-black text-black`
-                : `border-white text-white`
-            } z-50`}
-            onClick={handleMenu}
-          >
-            {showMenu === true ? "close" : "menu"}
-          </button>
-        </div>
-
+        <button
+          className={`border-2 rounded-4xl px-5 py-1.5 text-lg text-center cursor-pointer z-50 ${
+            showMenu === true
+              ? `border-black text-black`
+              : `border-white text-white`
+          }`}
+          onClick={handleMenu}
+        >
+          {showMenu === true ? "close" : "menu"}
+        </button>
         {showMenu && <MenuNav />}
-        {/* </div> */}
       </header>
     </div>
   );
--- src/components/MenuNav.jsx
+++ src/components/MenuNav.jsx
@@ -1,11 +1,11 @@
 function MenuNav() {
   return (
-    <aside className="absolute top-3 right-0 w-[35%] h-[90vh] mx-4 bg-white text-black rounded-2xl z-10">
+    <aside className="absolute top-3 right-0 w-[35%] h-[90vh] mx-4 bg-white text-black rounded-2xl z-0">
       <div className="flex flex-col px-6 pt-4 w-[100%]">
         <h1 className="mt-2 text-4xl font-bold text-gray-300">Menu</h1>
 
         <div className="flex flex-col justify-between">
-          <div className="w-0 mt-6 mb-50 pl-6 flex flex-col gap-6 text-3xl font-medium text-[#333] whitespace-nowrap">
+          <div className="w-0 mt-6 mb-50 pl-8 flex flex-col gap-6 text-3xl font-medium text-[#333] whitespace-nowrap">
             <a href="#">Home</a>
             <a href="#">Projects</a>
             <a href="#">Skill set</a>
@@ -13,7 +13,7 @@ function MenuNav() {
             <a href="#">Contact</a>
           </div>
 
-          <div className="w-0 pl-4 grid grid-cols-2 gap-40 font-medium text-[#333] whitespace-nowrap">
+          <div className="w-0 pl-4 grid grid-cols-2 gap-60 font-medium text-[#333] whitespace-nowrap">
             <div className="flex flex-col justify-between gap-8">
               <a href="#" className="transition-colors hover:text-[#555]">
                 LinkedIn
