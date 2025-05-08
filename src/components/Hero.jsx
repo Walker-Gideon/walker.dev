605f717600bae68a077d41a469c1df5b8f38ae19
Walker-Gideon <gideonawortwe3@gmail.com>
2025-05-08 18:03:36 +0000

    Added responsiveness to the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,15 +3,15 @@ import Button from "./Button";
 
 function Hero() {
   return (
-    <div className="pt-30 px-20 sm:px-8 md:px-10 lg:px-12 xl:px-16 w-full h-full flex flex-col justify-center gap-12">
-      <div className="w-full sm:text-center md:text-left">
-        <p className="text-4xl text-[#555] font-medium uppercase">
+    <div className="pt-30 small:pt-10 sm:pt-25 md:pt-30 px-20 small:px-4 sm:px-6 md:px-10 lg:px-12 xl:px-16 w-full h-full flex flex-col justify-center gap-12">
+      <div className="w-full small:text-center sm:text-center md:text-left">
+        <p className="text-4xl small:text-2xl sm:text-3xl md:text-4xl text-light font-medium uppercase">
           Fronted-end Web Developer
         </p>
-        <p className="font-bold sm:text-7xl lg:text-8xl md:text-7xl font-sans py-4">
+        <p className="font-bold small:text-4xl sm:text-5xl md:text-7xl lg:text-8xl font-sans py-4 md:py-6">
           Gideon Awortwe
         </p>
-        <p className="text-4xl text-gray-500 w-[70%] mb-10">
+        <p className="text-4xl small:text-3xl md:text-4xl text-gray-500 small:w-full md:w-[80%] lg:w-[70%] 2xl:w-[60%] mb-10">
           I build responsive, user-friendly websites with React, Tailwind CSS,
           and JavaScript — turning ideas into clean, functional code.
         </p>
