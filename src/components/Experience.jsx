b35061f7603af55706d256b5a5950fb9d0c4b372
Walker-Gideon <gideonawortwe3@gmail.com>
2025-05-09 00:44:21 +0000

    Added responsiveness to the experience section but have a bug to fix

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,20 +1,25 @@
 import React, { useState } from "react";
 import ExperienceContent from "./ExperienceContent";
 
-function Experienc() {
+function Experience() {
   const [checkClick, setCheckClick] = useState(false);
   const [checkFrameWork, setCheckFrameWork] = useState(false);
   const [checkCss, setCheckCss] = useState(false);
   const [checkGit, setCheckGit] = useState(false);
 
   return (
-    <div className="pt-20 mb-5 px-20 h-screen z-20">
-      <div className="pb-10">
-        <h1 className="text-4xl font-bold text-gray-400">Experience</h1>
-        <h1 className="text-4xl font-bold text-black">History</h1>
+    // small:px-4 sm:px-6 md:px-10 lg:px-20
+    <div className="pt-20 small:mt-10 mb-5 px-20 small:px-4 sm:px-6 md:px-10 lg:px-20 h-screen z-20">
+      <div className="pb-10 small:pb-6 sm:mb-8 medium:mb-10">
+        <h1 className="text-4xl small:text-2xl sm:text-3xl medium:text-4xl font-bold text-gray-400">
+          Experience
+        </h1>
+        <h1 className="text-4xl small:text-2xl sm:text-3xl medium:text-4xl font-bold text-black">
+          History
+        </h1>
       </div>
 
-      <div className="">
+      <div className="small:hidden md:block">
         <ExperienceContent
           check={checkClick}
           onCheck={() => setCheckClick(!checkClick)}
@@ -36,8 +41,31 @@ function Experienc() {
           title="Git & GitHub"
         />
       </div>
+
+      <div className="small:block md:hidden">
+        <SubExperience />
+      </div>
     </div>
   );
 }
 
-export default Experienc;
+export default Experience;
+
+export function SubExperience() {
+  const [checkClick, setCheckClick] = useState(true);
+  const [checkFrameWork, setCheckFrameWork] = useState(true);
+  const [checkCss, setCheckCss] = useState(true);
+  const [checkGit, setCheckGit] = useState(true);
+
+  return (
+    <div className="">
+      <ExperienceContent check={checkClick} title="HTML, CSS & Javascript" />
+      <ExperienceContent
+        check={checkFrameWork}
+        title="React.js as Javascript framework"
+      />
+      <ExperienceContent check={checkCss} title="Tailwind Css" />
+      <ExperienceContent check={checkGit} title="Git & GitHub" />
+    </div>
+  );
+}
--- src/components/ExperienceContent.jsx
+++ src/components/ExperienceContent.jsx
@@ -3,10 +3,10 @@ import { motion } from "motion/react";
 function ExperienceContent({ check, onCheck, title }) {
   const boxVariants = {
     initial: {
-      height: "15vh",
+      height: "12vh",
     },
     expanded: {
-      height: "30vh",
+      height: "20vh",
     },
   };
 
@@ -20,16 +20,16 @@ function ExperienceContent({ check, onCheck, title }) {
         ease: "easeOut",
         height: { duration: 0.4, ease: "easeOut", delay: 0.2 },
       }}
-      className="border-b border-black py-8 overflow-hidden"
+      className="border-b border-black py-8 small:py-2 small:mb-5 medium:mb-8 md:mb-0 md:py-8 overflow-hidden"
       onMouseEnter={onCheck}
       onMouseLeave={onCheck}
     >
-      <div className="flex items-center justify-between px-4">
-        <h1 className="text-2xl">{title}</h1>
+      <div className="flex items-center small:text-center small:justify-center md:justify-between px-4">
+        <h1 className="text-2xl small:text-2xl medium:text-3xl">{title}</h1>
       </div>
 
-      <div className="mt-8 px-13 text-black text-lg  transition-all duration-500 overflow-hidden">
-        <p className="py-4 transform transition-all duration-500">
+      <div className="mt-8 small:mt-1 medium:mt-2 md:mt-4 px-13 small:px-0.5 medium:px-4 md:px-10 text-black text-lg medium:text-xl md:text-lg transition-all duration-500 overflow-hidden">
+        <p className="py-4 small:py-1 medium:py-2 md:py-1 transform transition-all duration-500">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio deleniti
           dolor non nisi, quas id incidunt eligendi pariatur quos unde sapiente
           temporibus, consequatur in blanditiis. Aperiam dignissimos magni id
