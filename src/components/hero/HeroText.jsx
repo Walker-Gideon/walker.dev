3a91f8e388702abe43be0600219a0d20b5f0e2cc
Walker-Gideon <gideonawortwe3@gmail.com>
2025-05-14 22:39:49 +0000

    Did final touch to the hero section

--- src/components/hero/HeroText.jsx
+++ src/components/hero/HeroText.jsx
@@ -1,13 +1,13 @@
 function HeroText() {
   return (
     <div className="md:mb-15">
-      <p className="text-4xl small:text-2xl sm:text-3xl md:text-3xl text-blueprime font-medium uppercase">
+      <p className="text-3xl small:text-2xl medium:text-3xl text-blueprime font-medium uppercase">
         Fronted-end Web Developer
       </p>
-      <p className="font-bold small:text-5xl sm:text-6xl medium:text-7xl md:text-8xl font-sans py-4 small:py-2 md:py-6 text-primary">
+      <p className="font-bold text-8xl small:text-5xl medium:text-8xl font-sans py-4 small:py-2 md:py-6 text-primary">
         Gideon Awortwe
       </p>
-      <p className="text-2xl small:text-2xl text-white small:w-full md:w-[80%]  mb-10">
+      <p className="text-2xl text-white mb-10">
         I build responsive, user-friendly websites with React, Tailwind CSS, and
         JavaScript — turning ideas into clean, functional code.
       </p>
--- src/components/hero/Image.jsx
+++ src/components/hero/Image.jsx
@@ -3,7 +3,7 @@ import creatorImage from "../../assets/my-prof.jpeg";
 function Image() {
   return (
     <div className="small:p-4 sm:px-6 sm:p-0">
-      <div className="w-full h-full small:w-[280px] md:w-[300px] small:h-120 sm:w-full sm:h-150 medium:h-160 md:h-125 p-0.5 small:p-1 bg-primary/30 backdrop-blur-md justify-self-end rounded-xl rotate-10 small:rotate-2 sm:rotate-6 medium:rotate-8">
+      <div className="w-110 small:w-95 sm:w-105 medium:w-110 h-full p-0.5 small:p-1 bg-primary/30 backdrop-blur-md justify-self-end rounded-xl rotate-10 small:rotate-2 sm:rotate-6 medium:rotate-8">
         <div className="relative w-full h-140 small:h-full sm:h-full medium:h-160 md:h-full p-2 small:p-1 bg-blue/30 backdrop-blur-md rounded-xl -rotate-5">
           <img
             src={creatorImage}
