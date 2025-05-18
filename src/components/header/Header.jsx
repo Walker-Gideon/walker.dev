d0dd00a97dc623f9238a335df6741fc777e1ce4e
Gideon Awortwe Walker <gideonawortwe3@gmail.com>
2025-05-18 19:43:13 +0000

    Working on the header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,14 +7,14 @@ export default function Header() {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
-    <div className="w-full h-0 sm:px-6 md:px-6 lg:px-8 xl:px-8 py-4 sticky top-0 z-30">
+    <div className="w-full h-0 sm:px-6 md:px-6 lg:px-8 xl:px-10 py-4 sticky top-0 z-30">
       <header className="flex items-center justify-between">
         <Logo />
         <MenuButton showMenu={showMenu} setShowMenu={setShowMenu} />
       </header>
 
       <div className="absolute top-4 right-0 xl:right-8 lg:right-8 sm:right-6 md:right-6 overflow-hidden">
-        <MenuNav showMenu={showMenu} />
+        {/* <MenuNav showMenu={showMenu} /> */}
       </div>
     </div>
   );
--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -3,7 +3,7 @@ function Logo() {
     <div className="border-b-2 border-blueprime pb-0.5">
       <a
         href="#"
-        className="text-primary sm:text-lg md:text-xl lg:text-xl text-lg uppercase font-bold cursor-pointer"
+        className="text-primary text-lg uppercase font-bold cursor-pointer"
       >
         Wɑɭkɜɾ.dev
       </a>
