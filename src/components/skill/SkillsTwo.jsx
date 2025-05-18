1ed6a99e9a4611474c1c6c20ddcd41e2346932e6
Walker-Gideon <gideonawortwe3@gmail.com>
2025-05-18 02:55:05 +0000

    Added motion to the skills section

--- src/components/skill/SkillsTwo.jsx
+++ src/components/skill/SkillsTwo.jsx
@@ -2,6 +2,8 @@ import { RiTailwindCssFill } from "react-icons/ri";
 import { FaReact } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa6";
 import { TbBrandFramerMotion } from "react-icons/tb";
+import { fadeIn } from "../motion/variants";
+import { motion } from "motion/react";
 
 const skillsTwo = [
   {
@@ -27,7 +29,11 @@ function SkillsTwo() {
     <div className="flex w-full h- font-medium whitespace-nowrap ">
       <div className="relative w-full h-40 overflow-hidden skill_main decoration">
         {skillsTwo.map((skills, index) => (
-          <div
+          <motion.div
+            variants={fadeIn("up", `0.${index}`)}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: false, amount: 0.7 }}
             key={index}
             className={`absolute w-30 text-primary text-center animate_skill_two skill_delay_two${
               index + 1
@@ -39,7 +45,7 @@ function SkillsTwo() {
                 <li className="text-3xl">{skills.text}</li>
               </ul>
             </div>
-          </div>
+          </motion.div>
         ))}
       </div>
     </div>
