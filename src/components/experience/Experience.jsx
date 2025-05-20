c03943f5bfb2f66aac1a498f67d37ad2aaaa07aa
Gideon Awortwe Walker <gideonawortwe3@gmail.com>
2025-05-20 03:50:10 +0000

    I have settle the bug now

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,10 +1,6 @@
-import { FaArrowRightLong } from "react-icons/fa6";
 import ExperienceContent from "./ExperienceContent";
 import ExperienceHeader from "./ExperienceHeader";
-import { motion } from "motion/react";
-import { fadeIn } from "../motion/variants";
 
-/*
 const experience = [
   {
     title: "Web Development",
@@ -37,70 +33,22 @@ const experience = [
     ],
   },
 ];
-*/
-
-const experience = [
-  {
-    title: "Web Development",
-    subtitle: "Independent Projects",
-    date: "2023 - Present",
-    text: [
-      "Built responsive websites using HTML, CSS, JavaScript, and Tailwind.",
-      "Created interactive UIs with React and reusable components.",
-      "Used Git & GitHub to manage and track project changes.",
-    ],
-  },
-  {
-    title: "React & Tailwind",
-    subtitle: "Hands-On Practice",
-    date: "2024 - Present",
-    text: [
-      "Built dynamic UIs with React and clean component logic.",
-      "Used Tailwind CSS to design modern, responsive layouts.",
-      "Wrote scalable code with reusable structure and patterns.",
-    ],
-  },
-  {
-    title: "Continuous Growth",
-    subtitle: "Learning Daily",
-    date: "Ongoing",
-    text: [
-      "Studying advanced JavaScript and UI performance tips.",
-      "Following design principles to improve user experience.",
-      "Engaging in tutorials, dev groups, and open learning.",
-    ],
-  },
-];
 
 function Experience() {
   return (
     <div id="experience" className="w-full h-full pt-18 px-4 md:px-20">
       <ExperienceHeader />
 
-      <div className="w-full h-full flex flex-col midmedium:flex-row items-center justify-between gap-10 midmedium:gap-8 px-10 midmedium:px-8">
+      <div className="w-full h-full flex flex-col midmedium:grid midmedium:grid-cols-3 items-center justify-between gap-10 midmedium:gap-20 px-10 md:px-0 midmedium:px-8">
         {experience.map((exp, index) => (
-          <>
-            <ExperienceContent
-              key={index}
-              title={exp.title}
-              subtitle={exp.subtitle}
-              date={exp.date}
-              text={exp.text}
-              index={index}
-            />
-            {index < 2 ? (
-              <motion.div
-                variants={fadeIn("right", `0.${index}`)}
-                initial="hidden"
-                whileInView="show"
-                viewport={{ once: false, amount: 0 }}
-              >
-                <FaArrowRightLong className="text-3xl text-primary hidden midmedium:block" />
-              </motion.div>
-            ) : (
-              ""
-            )}
-          </>
+          <ExperienceContent
+            key={index}
+            title={exp.title}
+            subtitle={exp.subtitle}
+            date={exp.date}
+            text={exp.text}
+            index={index}
+          />
         ))}
       </div>
     </div>
